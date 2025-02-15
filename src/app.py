eba62b8d001417f172bb375549474e747ebec0b9
Braulio Solorio <soloriobraulio03@gmail.com>
2025-02-15 14:53:34 -0600

    first production line

--- src/app.py
+++ src/app.py
@@ -7,4 +7,14 @@ from WorkStation import WorkStation
 
 env = simpy.Environment()
 Factory = Factory(env)
+
+ProdLine1 = ProductionLine(env)
+
+for i in range(6):
+    ProdLine1.add_work_station(WorkStation(env))
+
+Factory.add_production_line(ProdLine1);
+
+print("=== Simulation Started ===")
 env.run(until=50)
+print("=== Simulation Finished ===")
